c985d46cb323cb7933328eac5bd3ac2ad59cf9a1
shiva <shivarahimi.f@gmail.com>
2024-12-25 13:35:31 +0330

    shiva-update

--- src/components/tools/Headers/Headers.tsx
+++ src/components/tools/Headers/Headers.tsx
@@ -4,7 +4,19 @@ import { FC } from "react";
 interface IPropType {}
 
 const Headers: FC<IPropType> = () => {
-  return <>Headers</>;
+  return (
+    <header>
+      <section className="flex items-center justify-between">
+        <section>logo</section>
+
+        <ul>
+          <li>menu</li>
+        </ul>
+
+        <section>search</section>
+      </section>
+    </header>
+  );
 };
 
 export { Headers };
