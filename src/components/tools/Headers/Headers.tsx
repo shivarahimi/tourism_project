67d280724217bef20951f8686fe429c5c8993449
shiva <shivarahimi.f@gmail.com>
2024-12-26 00:27:26 +0330

    shiva-update

--- src/components/tools/Headers/Headers.tsx
+++ src/components/tools/Headers/Headers.tsx
@@ -1,19 +1,35 @@
 // base
 import { FC } from "react";
+import { headerMenuList } from "@/core/data/HeaderMenu/HeaderMenu.data";
+import { BsSearch } from "react-icons/bs";
+import { HeaderResponsive } from "./HeaderResponsive/HeaderResponsive";
 
 interface IPropType {}
 
 const Headers: FC<IPropType> = () => {
   return (
-    <header>
-      <section className="flex items-center justify-between">
-        <section>logo</section>
+    <header className="bg-gray-800 text-white">
+      <section className="flex items-center justify-between w-[90%] mx-auto">
+        <section className="flex items-center justify-start">
+          <div className="md:hidden cursor-pointer">
+            <HeaderResponsive />
+          </div>
+          <div className="bg-gt-gradient-1">logo</div>
+        </section>
 
-        <ul>
-          <li>menu</li>
+        <ul className="hidden md:flex md:items-center md:justify-center gap-3">
+          {headerMenuList.map((item) => (
+            <>
+              <li key={item.key}>{item.title}</li>
+            </>
+          ))}
         </ul>
 
-        <section>search</section>
+        <section>
+          <div className="hidden md:block bg-gt-gradient-1 rounded-[40px] p-5">
+            <BsSearch />
+          </div>
+        </section>
       </section>
     </header>
   );
